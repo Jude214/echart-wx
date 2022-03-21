04a3d36461df51ffe4e101a37bd4193abb7445cd
祖德 <liaozude@zailingtech.com>
2022-03-21 10:48:17 +0800

    echart-wx.d.ts

--- echart-wx.d.ts
+++ echart-wx.d.ts
@@ -1,10 +1,32 @@
-/// <reference path="node_modules/@types/echarts/index.d.ts"/>
 
+
+/*  /// <reference path="node_modules/@types/echarts/index.d.ts"/>  */
 // export as namespace echarts
 // export default echarts
-// // export = echarts
 
-declare module 'echarts/types/echarts' {
+declare namespace echarts {
+  /**
+   * Creates an ECharts instance, and returns an echartsInstance. You shall
+   *     not initialize multiple ECharts instances on a single container.
+   *
+   * @param {HTMLDivElement | HTMLCanvasElement} dom Instance container,
+   *     usually is a `div` element with height and width defined.
+   * @param {object | string} [theme] Theme to be applied.
+   *     This can be a configuring object of a theme, or a theme name
+   *     registered through [echarts.registerTheme](https://echarts.apache.org/api.html#echarts.registerTheme).
+   * @param {object} [opts] Chart configurations.
+   * @param {number} [opts.devicePixelRatio] Ratio of one physical pixel to
+   *     the size of one device independent pixels. Browser's
+   *     `window.devicePixelRatio` is used by default.
+   * @param {string} [opts.renderer] Supports `'canvas'` or `'svg'`.
+   *     See [Render by Canvas or SVG](https://echarts.apache.org/tutorial.html#Render%20by%20Canvas%20or%20SVG).
+   * @param {number} [opts.width] Specify width explicitly, in pixel.
+   *     If setting to `null`/`undefined`/`'auto'`, width of `dom`
+   *     (instance container) will be used.
+   * @param {number} [opts.height] Specify height explicitly, in pixel.
+   *     If setting to `null`/`undefined`/`'auto'`, height of `dom`
+   *     (instance container) will be used.
+   */
   function init(
       dom: HTMLDivElement | HTMLCanvasElement,
       theme?: object | string,
@@ -1622,4 +1644,12 @@ declare module 'echarts/types/echarts' {
           }
       }
   }
+}
+
+declare module 'echarts' {
+  export = echarts;
+}
+
+declare module 'echarts/lib/echarts' {
+  export = echarts;
 }
